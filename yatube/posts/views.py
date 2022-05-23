662f2732a449bee22c9014248c1005412f24b0c1
Vladislav Guzov <tim.barr@yandex.ru>
2022-05-23 18:35:04 +0300

    Custom error pages

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -114,6 +114,8 @@ def follow_index(request):
 
 @login_required
 def profile_follow(request, username):
+    if request.user == get_object_or_404(User, username=username):
+        return redirect('posts:profile', username=username)
     follow = Follow(
         user=request.user,
         author=get_object_or_404(User, username=username)
